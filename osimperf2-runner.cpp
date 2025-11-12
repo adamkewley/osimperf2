fcceeeb26e36d6e345df657881168a76c8df6385
Adam Kewley <contact@adamkewley.com>
2025-11-12 15:30:25 +0100

    Refactor/clean up osimperf2-runner

--- osimperf2-runner.cpp
+++ osimperf2-runner.cpp
@@ -21,162 +21,100 @@
 using namespace OpenSim;
 using namespace SimTK;
 
-class Timer {
-	public:
+using PerfClock = std::chrono::steady_clock;
 
-    double measureDuration(
-            const std::string& label,
-            const std::function<void()>& f);
-
-    void storeResult(
-            const std::string& label,
-            double dt);
-
-    void print(std::ostream& os) const;
-
-private:
-    void tick();
-    double tock();
-
-    std::ostringstream m_Results;
-    std::chrono::time_point<std::chrono::high_resolution_clock> m_Tick;
-};
-
-
-void Timer::tick() { m_Tick = std::chrono::high_resolution_clock::now(); }
-
-double Timer::tock() {
-    std::chrono::time_point<std::chrono::high_resolution_clock> tock =
-        std::chrono::high_resolution_clock::now();
-    const double dt = std::chrono::duration<double>(tock - m_Tick).count();
-    m_Tick = std::chrono::time_point<std::chrono::high_resolution_clock>::max();
-    return dt;
-}
-
-double Timer::measureDuration(
-        const std::string& label,
-        const std::function<void()>& f) {
-    tick();
-    f();
-    const double dt = tock();
-    storeResult(
-            label, dt
-            );
-    return dt;
-}
-
-void Timer::storeResult(
-    const std::string& label,
-    double dt)
+namespace
 {
-    m_Results << label << "," << dt << "\n";
-}
+    PerfClock::duration runToolIK(
+        const std::string& modelFile,
+        const std::string& toolFile,
+        bool visualize)
+    {
+        Model model(modelFile);
+        if (visualize) { model.setUseVisualizer(true); }
 
-void Timer::print(std::ostream& os) const {
-    os << m_Results.str();
-    os.flush();
-}
+        OpenSim::InverseKinematicsTool tool(toolFile, false);
+        tool.setModel(model);
 
-void printHeader(const std::string& label, std::ostream& os) {
-    std::cout << "\n";
-    std::cout << "==============================================================\n";
-    std::cout << "====================== " << label << " ====================\n";
-    std::cout << "==============================================================\n";
-    std::cout << std::endl;
-}
-
-Real runToolIK(
-        Timer& timer,
-        const std::string& modelFile, const std::string& toolFile,
-        bool visualize) {
-
-    printHeader("Inverse Kinematics", std::cout);
-
-    Model model(modelFile);
-    if (visualize) { model.setUseVisualizer(true); }
-
-    OpenSim::InverseKinematicsTool tool(toolFile, false);
-    tool.setModel(model);
-
-    const std::string label = model.getName() + "_IK";
-    const Real dt = timer.measureDuration(label, [&]() { tool.run(); });
-
-    return dt;
-}
-
-Real runToolCMC(
-        Timer& timer,
-        const std::string& modelFile, const std::string& toolFile,
-        bool visualize) {
-
-    printHeader("Computed Muscle Control", std::cout);
-
-    Model model(modelFile);
-    if (visualize) { model.setUseVisualizer(true); }
-
-    CMCTool tool(toolFile, false);
-    tool.setModel(model);
-    tool.setFinalTime(0.65);
-
-    const std::string label = model.getName() + "_CMC";
-    const Real dt = timer.measureDuration(label, [&]() { tool.run(); });
-
-    return dt;
-}
-
-Real runToolFwd(Timer& timer, const std::string& modelFile, const std::string& toolFile,
-        bool visualize) {
-
-    printHeader("Forward Dynamics", std::cout);
-
-    std::cout << "modelFile = " << modelFile << ", toolFile = " << toolFile << "\n";
-    ForwardTool tool(toolFile, true, false);
-
-    Model model(modelFile);
-    if (visualize) { model.setUseVisualizer(true); }
-
-    tool.setModel(model);
-
-    const std::string label = model.getName() + "_Fwd";
-    const Real dt = timer.measureDuration(label, [&]() { tool.run(); });
-
-    return dt;
-}
-
-SimTK::Real runAndTime(
-        Timer& timer,
-        const std::string& modelFile, bool visualize, SimTK::Real finalTime) {
-
-    printHeader("Run Integrator", std::cout);
-
-    Model model(modelFile);
-    model.setUseVisualizer(visualize);
-    SimTK::State& s = model.initSystem();
-    s.setTime(0.);
-    model.realizeReport(s);
-
-    SimTK::RungeKuttaMersonIntegrator integrator(model.getSystem());
-    integrator.setInternalStepLimit(20000);
-    integrator.setMinimumStepSize(1.0e-8    );
-    integrator.setMaximumStepSize(1.0);
-    integrator.setAccuracy(1.0e-5);
-    integrator.setFinalTime(finalTime);
-    // integrator.setReturnEveryInternalStep(true);  // so that cancellations/interrupts work
-    integrator.initialize(s);
-
-    SimTK::TimeStepper stepper{model.getSystem(), integrator};
-    stepper.initialize(s);
-    // stepper.setReportAllSignificantStates(true);  // so that cancellations/interrupts work
-
-    const Real dt = timer.measureDuration(
-        model.getName() + "_run",
-        [&]() { stepper.stepTo(finalTime); });
-    const Real finalIntegratorTime = integrator.getTime();
-    if (finalIntegratorTime < finalTime) {
-        throw std::runtime_error("did not reach final time");
+        const auto begin = PerfClock::now();
+        tool.run();
+        return PerfClock::now() - begin;
     }
-    std::flush(std::cout);
-    return dt;
+
+    PerfClock::duration runToolCMC(
+        const std::string& modelFile,
+        const std::string& toolFile,
+        bool visualize)
+    {
+        Model model(modelFile);
+        if (visualize) { model.setUseVisualizer(true); }
+
+        CMCTool tool(toolFile, false);
+        tool.setModel(model);
+        tool.setFinalTime(0.65);
+
+        const std::string label = model.getName() + "_CMC";
+
+        auto before = PerfClock::now();
+        tool.run();
+        return PerfClock::now() - before;
+    }
+
+    PerfClock::duration runToolFwd(
+        const std::string& modelFile,
+        const std::string& toolFile,
+        bool visualize)
+    {
+        std::cout << "modelFile = " << modelFile << ", toolFile = " << toolFile << "\n";
+        ForwardTool tool(toolFile, true, false);
+
+        Model model(modelFile);
+        if (visualize) { model.setUseVisualizer(true); }
+
+        tool.setModel(model);
+
+        const std::string label = model.getName() + "_Fwd";
+        const auto before = PerfClock::now();
+        tool.run();
+        return PerfClock::now() - before;
+    }
+
+    PerfClock::duration runAndTime(
+            const std::string& modelFile,
+            bool visualize,
+            SimTK::Real finalTime)
+    {
+        Model model(modelFile);
+        model.setUseVisualizer(visualize);
+        SimTK::State& s = model.initSystem();
+        s.setTime(0.);
+        model.realizeReport(s);
+
+        // The choice of integrator/parameters matches what OpenSim Creator and
+        // OpenSim GUI do by default.
+        SimTK::RungeKuttaMersonIntegrator integrator(model.getSystem());
+        integrator.setInternalStepLimit(20000);
+        integrator.setMinimumStepSize(1.0e-8    );
+        integrator.setMaximumStepSize(1.0);
+        integrator.setAccuracy(1.0e-5);
+        integrator.setFinalTime(finalTime);
+        // integrator.setReturnEveryInternalStep(true);  // so that cancellations/interrupts work
+        integrator.initialize(s);
+
+        SimTK::TimeStepper stepper{model.getSystem(), integrator};
+        stepper.initialize(s);
+        // stepper.setReportAllSignificantStates(true);  // so that cancellations/interrupts work
+
+        const auto before = PerfClock::now();
+        stepper.stepTo(finalTime);
+        const auto duration = PerfClock::now() - before;
+
+        if (integrator.getTime() < finalTime) {
+            throw std::runtime_error("did not reach final time");
+        }
+
+        return duration;
+    }
 }
 
 int main(int argc, char* argv[]) {
@@ -216,42 +154,30 @@ int main(int argc, char* argv[]) {
         RegisterTypes_osimSimulation();
     #endif
 
-    Timer timer;
+    PerfClock::duration dt{};
 
     if (run) {
-        SimTK::Real dt = runAndTime(timer, model, visualize, simTime);
-
-        std::cout << "RESULTS:\n";
-        std::cout << "--> dt = " << dt << "\n";
+        dt = runAndTime(model, visualize, simTime);
     }
 
     if (!cmc.empty()) {
-        SimTK::Real dt = runToolCMC(timer, model, cmc, visualize);
-
-        std::cout << "RESULTS:\n";
-        std::cout << "--> dt = " << dt << "\n";
+        dt = runToolCMC(model, cmc, visualize);
     }
 
     if (!fwd.empty()) {
-        SimTK::Real dt = runToolFwd(timer, model, fwd, visualize);
-
-        std::cout << "RESULTS:\n";
-        std::cout << "--> dt = " << dt << "\n";
+        dt = runToolFwd(model, fwd, visualize);
     }
 
     if (!ik.empty()) {
-        SimTK::Real dt = runToolIK(timer, model, ik, visualize);
-
-        std::cout << "RESULTS:\n";
-        std::cout << "--> dt = " << dt << "\n";
+        dt = runToolIK(model, ik, visualize);
     }
 
-    std::cout << "Printing results to " << out << std::endl;
+    const double dseconds = std::chrono::duration_cast<std::chrono::duration<double>>(dt).count();
 
-    std::ofstream oFile(out, std::fstream::app);
-    timer.print(oFile);
-    oFile.close();
+    std::cout << "RESULTS:\n";
+    std::cout << "--> dt = " << dseconds << "\n";
+
+    // TODO: output dseconds to somewhere sane
 
     return 0;
 }
-
